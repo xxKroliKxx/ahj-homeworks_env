458296be69be4960e71c34c25ab862ae0e9f4ed0
rka <russkikh_k@ekb.sima-land.ru>
2022-01-03 00:25:10 +0400

    +

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,15 @@
 const { merge } = require('webpack-merge');
-// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common');
 // const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    // minimize: true,
-    // minimizer: [
-    //   new OptimizeCSSAssetsPlugin({}),
-    //   '...',
-    // ],
-
+    minimize: true,
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+      '...',
+    ],
   },
 });
